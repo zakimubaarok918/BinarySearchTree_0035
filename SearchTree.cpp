36b045b3c6ea90a80319bb12db04e56fce6c1dd4
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-11 16:08:46 +0700

    add case for inorder traversal in main menu

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -156,3 +156,10 @@ int main()
                 obj.insert(word);
                 break;
             }      
+
+        case '2':
+            {
+                obj.inorder(obj.ROOT);
+                cout << endl;
+                break;
+            }    
