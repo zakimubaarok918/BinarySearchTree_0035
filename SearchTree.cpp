b505551059f496bdf7140ba29ffcbb18346b903d
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-11 15:51:16 +0700

    implement search method in BinaryTree class

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -57,3 +57,21 @@ public:
              parent->rightchild = newNode;
          }
     }
+
+    // This function searches the current node of the specified node
+    // as well as the current node of its parrent
+    void search(string element, Node *&parent, Node *&currentNode)
+    {
+        currentNode = ROOT;
+        parent = NULL;
+
+        while ((currentNode != NULL) && (currentNode->info != element))
+        {
+            parent = currentNode;
+
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;    
+        }
+    }    
