94e2ebe878cce185a705ec7e8fcd5f1e0f711639
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-06-11 15:11:57 +0700

    implement Node class with constructor for binary search tree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node 
+{
+public:
+    string data;
+    Node* leftchild;
+    Node* rightchild;
+
+    // Constructor for the node class
+    Node(string i, Node *l, Node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};    
